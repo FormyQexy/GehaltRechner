b6566857a793c6f2747e44a6fc43b2e64b53eafe
FormyQexy <121886507+FormyQexy@users.noreply.github.com>
2023-01-03 18:02:18 +0100

    Rechner

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-for x in range(1, 101):
-    print(x)
+Stundenlohn = input("Bitte gebe deinen Stundenlohn ein:")
+tag = 8 * 24
+Monat = 28 * 192
+Jahr = 12 * 5376
+ZehnJahre = 10 * 64512
+
+print("Dein Stundenlohn beträgt " + str(Stundenlohn) + "€")
+print("Du verdienst " + str(tag) + " € pro Tag")
+print("Du verdienst " + str(Monat) + "€ im Monat ")
+print("Du verdienst " + str(Jahr) + "€ im Jahr")
+print("Du würdest in 10 Jahren " + str(ZehnJahre) + "€ verdienen")
+input("Drücke eine belibige Taste um das Programm zu schließen!")
